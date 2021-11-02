06e6ee441c2f4d8ad543c7adf49195432295ec4d
vihowe <vihowe@qq.com>
2021-11-02 22:33:58 +0800

    plot performance surface

--- analyse.py
+++ analyse.py
@@ -5,8 +5,9 @@ import matplotlib
 from matplotlib import pyplot as plt
 
 # %%
-df = pd.read_csv('./perf_surf.csv', skip_blank_lines=True)
+df = pd.read_csv('./p100.csv', skip_blank_lines=True)
 x = list(dict.fromkeys(df['cpu_quota'].tolist()).keys())
+x = [i/100000 for i in x]
 y = list(dict.fromkeys(df['mem_quota'].tolist()).keys())
 x, y = np.meshgrid(y, x)
 qps = np.array(df['qps'])
