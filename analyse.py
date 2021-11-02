604eea36735ec7117d9b0b44d18ce5eaef5df877
vihowe <vihowe@qq.com>
2021-11-02 16:59:50 +0800

    plot performance surface

--- analyse.py
+++ analyse.py
@@ -2,10 +2,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
 # %%
 df = pd.read_csv('./perf_surf.csv', skip_blank_lines=True)
-x = dict.fromkeys(df['cpu_quota'].tolist())
-y = dict.fromkeys(df['mem_quota'].tolist())
+x = list(dict.fromkeys(df['cpu_quota'].tolist()).keys())
+y = list(dict.fromkeys(df['mem_quota'].tolist()).keys())
+x, y = np.meshgrid(y, x)
+qps = np.array(df['qps'])
+qps = np.reshape(qps, x.shape)
 print(x, y)
+fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+surf = ax.plot_surface(x, y, qps)
